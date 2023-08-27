75ff0f758419f0c1d964039138391f856a133287
kkhrh <k.khatib@rightcode.io>
2023-08-27 16:56:33 +0300

    صفحة_انشاء_حساب_تم

--- lib/src/auth/presentation/components/sgin_up_with_email_component.dart
+++ lib/src/auth/presentation/components/sgin_up_with_email_component.dart
@@ -2,7 +2,6 @@
   import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:icons_plus/icons_plus.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 import 'package:skill_race/core/presentation/widget/dynamic-Input.dart';
 import 'package:skill_race/core/presentation/widget/dynamic_button.dart';
